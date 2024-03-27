16ba55762f3801a712464dd76be387fc42058f41
nhatminhtrieu <trieunhatminh2003@gmail.com>
2024-03-27 08:23:25 +0700

    POL-hotfix-1 update gitignore

--- app/src/main/java/com/example/polary/MainActivity.kt
+++ app/src/main/java/com/example/polary/MainActivity.kt
@@ -11,7 +11,6 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.polary.ui.theme.PolaryTheme
 
 class MainActivity : AppCompatActivity() {
