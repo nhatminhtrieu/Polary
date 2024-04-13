8d42586d002a8f50c771293bb111c7e059ebb1c2
Quan0308 <bddquan@gmail.com>
2024-04-13 10:51:58 +0700

    POL-43 feat: display detail of reactions

--- app/src/main/java/com/example/polary/PostView/ReactionFragment.kt
+++ app/src/main/java/com/example/polary/PostView/ReactionFragment.kt
@@ -12,26 +12,26 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.polary.Class.HttpMethod
 import com.example.polary.R
 import com.example.polary.dataClass.Comment
+import com.example.polary.dataClass.Reaction
 import com.example.polary.utils.ApiCallBack
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
-class CommentFragment(private val postId: Number) : BottomSheetDialogFragment() {
+class ReactionFragment(private val postId: Number): BottomSheetDialogFragment() {
     private var Behavior: BottomSheetBehavior<FrameLayout>? = null
-    private lateinit var _comments: List<Comment>
-    private lateinit var rvComment: RecyclerView
-    private lateinit var commentAdapter: CommentAdapter
+    private lateinit var _reactions: List<Reaction>
+    private lateinit var rvReaction: RecyclerView
+    private lateinit var reactionAdapter: ReactionAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setStyle(STYLE_NORMAL, R.style.BottomSheetDialogStyle)
     }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.comment, container, false)
-        getComments(postId)
+        val view = inflater.inflate(R.layout.reaction, container, false)
+        getReactions(postId)
         return view
     }
-
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return BottomSheetDialog(requireContext(), R.style.BottomSheetDialogStyle).apply {
             setContentView(R.layout.comment)
@@ -41,8 +41,7 @@ class CommentFragment(private val postId: Number) : BottomSheetDialogFragment() 
                 Behavior = BottomSheetBehavior.from(bottomSheet)
             }
 
-
-            view?.findViewById<RecyclerView>(R.id.comment_recycler_view)?.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            view?.findViewById<RecyclerView>(R.id.reaction_recycler_view)?.addOnScrollListener(object : RecyclerView.OnScrollListener() {
                 override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
                     super.onScrollStateChanged(recyclerView, newState)
                     if(Behavior is CustomBottomSheetBehavior) {
@@ -53,18 +52,18 @@ class CommentFragment(private val postId: Number) : BottomSheetDialogFragment() 
         }
     }
 
-    private fun getComments(postId: Number) {
-        // get comments from server
+    private fun getReactions(postId: Number) {
+        // get reactions from server
         val httpMethod = HttpMethod()
-        httpMethod.doGet<Comment>("posts/${postId}/comments", object: ApiCallBack<Any> {
+        httpMethod.doGet<Reaction>("posts/${postId}/reactions", object: ApiCallBack<Any> {
             override fun onSuccess(data: Any) {
-                val comments = data as List<Comment>
-                _comments = comments
-                rvComment = view?.findViewById(R.id.comment_recycler_view)!!
-                commentAdapter = CommentAdapter(comments)
-                rvComment.layoutManager = LinearLayoutManager(context)
-                rvComment.adapter = commentAdapter
-                rvComment.setHasFixedSize(true)
+                val reactions = data as List<Reaction>
+                _reactions = reactions
+                rvReaction = view?.findViewById(R.id.reaction_recycler_view)!!
+                reactionAdapter = ReactionAdapter(reactions)
+                rvReaction.layoutManager = LinearLayoutManager(context)
+                rvReaction.adapter = reactionAdapter
+                rvReaction.setHasFixedSize(true)
             }
             override fun onError(error: Throwable) {
                 Log.e("error", "Error: ${error.message}")
